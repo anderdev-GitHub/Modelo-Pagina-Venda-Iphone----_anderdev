4bbe05f00038d2f53642c3500072658404d82e7a
AnderDev513 <andersondiasleite@icloud.com>
2023-03-20 02:35:35 -0300

    Adição de JS

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+function trocaCor(cor) {
+
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaIphone(imagem) {
+    
+    let imgIphone = document.querySelector(".img-iphones")
+    imgIphone.src = imagem
+
+}
